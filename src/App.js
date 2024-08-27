a10ed84fb1359fe13d0a97d56987d0d6f9ea05f7
BHARATHKUMAR K <115342579+Bharath0063@users.noreply.github.com>
2024-08-27 19:46:25 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './App.css';
+import Chess from './components/chess';
+
+const App=()=>{
+    return(
+        <>
+       <Chess/>
+        </>
+    )
+}
+export default App
